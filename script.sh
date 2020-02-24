0fdaba4d94edac87f3c8b8aaa20a269f8be52f42
João Canoso <jpcanoso@hotmail.com>
2020-02-24 12:33:04 +0000

    Finished MongoDB setup
    Fixed a missing MySQL restart
    Added MongoDB user
    Added MongoDB Auth to daemon
    Replaced apt with apt-get for stable CLI interface

--- script.sh
+++ script.sh
@@ -11,24 +11,37 @@ wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add 
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
 
 echo "====================== UPDATE & UPGRADE ====================="
-apt update -q
-apt dist-upgrade -y -q
-apt autoremove -y -q
-apt install software-properties-common git
+apt-get update -q
+# Lets speed up the process
+# apt dist-upgrade -y -q
+# apt autoremove -y -q
+apt-get install software-properties-common git
 
 # MongoDB
 echo "==================== INSTALLING MongoDB ====================="
-apt install -y -q mongodb-org
+apt-get install -y -q mongodb-org
+# DB directory
+mkdir -p /data/db
 # Start services
-sudo systemctl start mongod
-sudo systemctl enable mongod
+systemctl start mongod
+systemctl enable mongod
+# Add root user
+mongo << 'EOF'
+use admin
+db.createUser({user:"admin", pwd:"admin", roles:[{role:"root", db:"admin"}]})
+EOF
+# Add Auth to daemon
+sed -i "s/ExecStart=.*/ExecStart=\/usr\/bin\/mongod --auth --config \/etc\/mongod.conf/" /lib/systemd/system/mongod.service
+# Expose
+sed -i "s/  bindIp.*/  bindIp: 0.0.0.0/" /etc/mongod.conf
 
-# MariaDB
-echo "==================== INSTALLING MariaDB ====================="
-apt install -y -q mysql-server
-echo "=================== CONFIGURING MariaDB ====================="
-sudo sed -i -e 's/bind-addres/#bind-address/g' /etc/mysql/mysql.conf.d/mysqld.cnf
-sudo sed -i -e 's/skip-external-locking/#skip-external-locking/g' /etc/mysql/mysql.conf.d/mysqld.cnf
+# MySQL
+echo "==================== INSTALLING MySQL ====================="
+apt-get install -y -q mysql-server
+echo "=================== CONFIGURING MySQL ====================="
+# Expose
+sed -i -e 's/bind-addres/#bind-address/g' /etc/mysql/mysql.conf.d/mysqld.cnf
+sed -i -e 's/skip-external-locking/#skip-external-locking/g' /etc/mysql/mysql.conf.d/mysqld.cnf
 mysql -u root -proot -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'root'; FLUSH privileges;"
 # dashboard sql
 mysql -u root -proot -e "CREATE DATABASE laravel;"
@@ -38,13 +51,14 @@ echo "==================== INSTALLING NODEJS ====================="
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-apt update -q
-apt install -y -q nodejs yarn openssl libssl-dev
+apt-get update -q
+apt-get install -y -q nodejs yarn openssl libssl-dev
 
-# PHP STUFF
+# PHP 7.4
 echo "==================== INSTALLING PHP 7.4 ====================="
-apt install -y -q composer php7.4-fpm php7.4-common php7.4-mysql php7.4-xml php7.4-xmlrpc php7.4-curl php7.4-gd php7.4-imagick php7.4-cli php7.4-dev php7.4-imap php7.4-mbstring php7.4-opcache php7.4-soap php7.4-zip php-mongodb unzip
+apt-get install -y -q composer php7.4-fpm php7.4-common php7.4-mysql php7.4-xml php7.4-xmlrpc php7.4-curl php7.4-gd php7.4-imagick php7.4-cli php7.4-dev php7.4-imap php7.4-mbstring php7.4-opcache php7.4-soap php7.4-zip php-mongodb unzip
 echo "==================== CONFIGURING PHP ========================"
+# Config
 sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/7.4/fpm/php.ini
 sed -i "s/display_errors = .*/display_errors = On/" /etc/php/7.4/fpm/php.ini
 sed -i "s/upload_max_filesize = .*/upload_max_filesize = 20M/" /etc/php/7.4/fpm/php.ini
@@ -55,7 +69,7 @@ sed -i "s/group = .*/group = vagrant/" /etc/php/7.4/fpm/pool.d/www.conf
 
 # Nginx
 echo "==================== INSTALLING NGINX ====================="
-apt install -y -q nginx
+apt-get install -y -q nginx
 echo "==================== CONFIGURING NGINX ===================="
 cp /vagrant/nginx-default.conf /etc/nginx/conf.d/default.conf
 
@@ -68,7 +82,7 @@ dpkg-reconfigure --frontend noninteractive tzdata
 # Deploy
 echo "========================= DEPLOY ==========================="
 cd /vagrant
-# dashboard
+# Dashboard
 git clone https://github.com/mripta/dashboard.git
 cd dashboard
 composer install
@@ -76,7 +90,7 @@ cp .env.example .env
 sed -i "s/DB_PASSWORD=.*/DB_PASSWORD=root/" .env
 php artisan key:generate
 php artisan migrate
-# broker
+# Broker
 cd ..
 git clone https://github.com/mripta/broker.git
 cd broker
@@ -84,9 +98,11 @@ npm install
 
 # Restart Services
 echo "=================== RESTARTING SERVICES ===================="
-service nginx restart
+systemctl daemon-reload
+service mongod restart
+service mysql restart
 service php7.4-fpm restart
-#service mysql restart
+service nginx restart
 
 echo "========================== VERSIONS ========================"
 echo "NPM " `npm -v`
