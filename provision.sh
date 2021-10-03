0f5e707e3a5e9dc75448783466ad640555e77da5
Henrique Bernardo <henribernardo@hotmail.com>
2021-10-03 19:32:11 +0100

    Fix last commit

--- provision.sh
+++ provision.sh
@@ -11,6 +11,8 @@ debconf-set-selections <<< 'mysql-server mysql-server/root_password_again passwo
 curl -s http://deb.nodesource.com/gpgkey/nodesource.gpg.key | sudo apt-key add -
 sudo sh -c "echo deb http://deb.nodesource.com/node_14.x focal main > /etc/apt/sources.list.d/nodesource.list"
 #curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 
 # Add MongoDB Repo
 wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
